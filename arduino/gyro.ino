3f0fc17a08a24e6f61040cd7455165fbeff7b055
abbmarand <markus.andersson@abbgymnasiet.se>
2024-05-21 09:30:32 +0200

    Struntar i första värdet så den blir kalibrerad

--- arduino/gyro.ino
+++ arduino/gyro.ino
@@ -1,7 +1,8 @@
 
-
 #include <Wire.h>
 float RateRoll, RatePitch, RateYaw;
+int16_t initx, inity, initz;
+bool first = true;
 void gyro_signals(void) {
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
@@ -18,10 +19,36 @@ void gyro_signals(void) {
   int16_t GyroX=Wire.read()<<8 | Wire.read();
   int16_t GyroY=Wire.read()<<8 | Wire.read();
   int16_t GyroZ=Wire.read()<<8 | Wire.read();
-  RateRoll=(float)GyroX/65.5;
-  RatePitch=(float)GyroY/65.5;
-  RateYaw=(float)GyroZ/65.5;
+  RateRoll=(float)(GyroX-initx)/65.5;
+  RatePitch=(float)(GyroY-inity)/65.5;
+  RateYaw=(float)(GyroZ-initz)/65.5;
 }
+
+void init_gyro_signals(void) {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B); 
+  Wire.write(0x8); 
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68,6);
+  int16_t GyroX=Wire.read()<<8 | Wire.read();
+  int16_t GyroY=Wire.read()<<8 | Wire.read();
+  int16_t GyroZ=Wire.read()<<8 | Wire.read();
+  initx = GyroX;
+  inity = GyroY;
+  initz = GyroZ;
+  RateRoll=(float)0/65.5;
+  RatePitch=(float)0/65.5;
+  RateYaw=(float)0/65.5;
+  first = false;
+}
+
 void setup() {
   Serial.begin(57600);
   pinMode(13, OUTPUT);
@@ -33,14 +60,19 @@ void setup() {
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
+  init_gyro_signals();
 }
 void loop() {
-  gyro_signals();
+  if(first){
+    init_gyro_signals();
+  }else{
+    gyro_signals();
+  }
   Serial.print("Roll rate [°/s]= ");
   Serial.print(RateRoll);
   Serial.print(" Pitch Rate [°/s]= ");
   Serial.print(RatePitch);
   Serial.print(" Yaw Rate [°/s]= ");
   Serial.println(RateYaw);
-  delay(50);
+  delay(1000);
 }
